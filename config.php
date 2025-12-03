ac4ff8a60a7bd6ac8d6285bed35220b1d0fc8b46
camarada_nikc <nicole.p.goncalves@aluno.senai.br>
2025-12-03 16:46:03 -0300

    aula dia 3 do 10

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// configurações do banco de dados 
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'financeiro');
+define('DB_USER', 'root');
+define('DB_PASS', 'mysql');
+
+//conecxão com o banco de dados
+try {
+    $conn = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";
+    charset=utf8mb4",DB_USER,DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conecxão com o banco de dados " . $e->getMessage());
+}
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config.php';
+
+// verificar se o usuario está logado
+if (!isset($_SESSION['usuario_id'])){
+    header ('Location: login.php');
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Financeiro</title>
+</head>
+<body>
+    <h1>Sistema Financeiro</h1>
+</body>
+</html>
