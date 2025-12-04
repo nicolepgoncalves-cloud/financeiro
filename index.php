cda35f7585b50592289bfb50a5c439adc2902da7
camarada_nikc <nicole.p.goncalves@aluno.senai.br>
2025-12-04 09:37:23 -0300

    autenticar login

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if (!isset($_SESSION['usuario_id'])){
     header ('Location: login.php');
     exit;
 }
+
+$usuario_id = $_SESSION['usuario_id'];
+$usuario_nome = $_SESSION['usuario_nome'];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -16,5 +19,9 @@ if (!isset($_SESSION['usuario_id'])){
 </head>
 <body>
     <h1>Sistema Financeiro</h1>
+
+    <div>
+        <p>Bem-vindo. <strong> <?php echo $usuario_nome ?> </strong></p>
+    </div>
 </body>
 </html>
